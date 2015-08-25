a80b8782ca7052c6f29072771c813907ef40b4d2
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2015-08-25 09:01:19 -0300

    alterado para input.h

--- mac300/ep1/input.c
+++ mac300/ep1/input.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "input.h"
+
 int le_dimensao ( FILE *arq );
 
-double **destroi_Matriz ( FILE *arq, int dimensao, double **matriz );
+double **destroi_Matriz ( int dimensao, double **matriz );
 
 double **cria_Matriz ( FILE *arq, int dimensao );
 
-double **destroi_b ( FILE *arq, double *b );
+double **destroi_b ( double *b );
 
 double **cria_b ( FILE *arq, int dimensao );
