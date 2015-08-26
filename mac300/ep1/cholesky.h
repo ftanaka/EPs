55710567b0ab48aa852b5a67c8014c9841f33f90
Fernando Tanaka <fernando.ty.tanaka@gmail.com>
2015-08-26 13:54:00 -0300

    interface dos metodos cholesky

---
+++ mac300/ep1/cholesky.h
@@ -0,0 +1,11 @@
+int cholcol ( int, double ** );
+
+int forwcol ( int, double **, double * );
+
+int backcol ( int, double **, double *, int );
+
+int cholrow ( int, double ** );
+
+int forwrow ( int, double **, double * );
+
+int backrow ( int, double **, double *, int );
