1fd6224cb330143bf0451e506a311603ea8dc300
Fernando Tanaka <fernando.tomio.tanaka@gmail.com>
2015-08-26 09:52:33 -0300

    adicionado os imprime_*

--- mac300/ep1/input.h
+++ mac300/ep1/input.h
@@ -4,6 +4,10 @@ double **destroi_matriz ( int, double ** );
 
 double **cria_matriz ( FILE *, int );
 
+void imprime_matriz ( int, double ** );
+
 double *destroi_b ( double * );
 
 double *cria_b ( FILE *, int );
+ 
+void imprime_b ( int, double * );
