59d6bbd59b65ecad5e3dec421a0952dfeb016eab
Fernando Tanaka <fernando.ty.tanaka@gmail.com>
2015-09-01 23:46:09 -0300

    alterei para testar as funcoes de cholesky

--- mac300/ep1/main.c
+++ mac300/ep1/main.c
@@ -27,7 +27,7 @@ int main ( int argc, char **argv ) {
    imprime_b ( dimensao, b );
    printf ( "\n" );
 
-   retorno = cholcol ( dimensao, A );
+   retorno = cholrow ( dimensao, A );
 
    imprime_matriz ( dimensao, A );
 
