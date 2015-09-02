8f252663a1a88235baeef9ff7ed93934489a0d68
Fernando Tanaka <fernando.ty.tanaka@gmail.com>
2015-09-01 23:47:44 -0300

    metodos cholrow e cholcol implementados

--- mac300/ep1/cholesky.c
+++ mac300/ep1/cholesky.c
@@ -8,12 +8,11 @@ int cholcol ( int n, double **A ) {
    int i, j, k;
 
    for ( j = 0; j < n; j++ ) {
-
       for ( k = 0; k < j; k++ )
-         for ( i = 0; i <= j; i++ )
-            A[i][j] = A[i][j] - A[i][k] * A[j][k]; 
+         for ( i = j; i < n; i++ )
+            A[i][j] = A[i][j] - A[i][k] * A[j][k];
 
-      if ( A[j][j] <= 0 ) /* matriz A nao e definida positiva */
+      if ( A[j][j] <= 0 )
          return -1;
 
       A[j][j] = sqrt ( A[j][j] );
@@ -29,10 +28,18 @@ int cholrow ( int n, double **A ) {
    int i, j, k;
 
    for ( i = 0; i < n; i++ ) {
+      for ( j = 0; j <= i; j++ ) {
+         for ( k = 0; k < j; k++ )
+            A[i][j] = A[i][j] - A[i][k] * A[j][k];
 
-      for ( j = 0; j < i; j++ )
-         A[i][i] = sqrt ( A[i][i] - A[i][j] * A[i][j] );
-
+         if ( i == j ) {
+            if ( A[i][i] <= 0 )
+               return -1;
+            else
+               A[i][i] = sqrt ( A[i][i] );
+         } else
+            A[i][j] = A[i][j] / A[j][j];
+      }
    }
 
    return 0;
